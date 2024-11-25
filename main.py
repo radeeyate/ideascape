f0ad00f60bf9c4932ac01909bb6dd36d09867356
radeeyate <124477460+radeeyate@users.noreply.github.com>
2024-11-25 09:56:39 -0700

    made separate pages for specific languages and refactored the code in the jinja2 code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.exception_handler(404)
 async def not_found_exception_handler(request: Request, exc: HTTPException):
     return templates.TemplateResponse("404.html", {"request": request})
@@ -26,6 +27,199 @@ async def root(request: Request):
             "idea": idea["idea"],
         },
     )
+
+
+@app.get("/python")
+async def python(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Python"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "python",
+            "lang": "Python",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/cpp")
+async def cpp(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "C++"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "c++",
+            "lang": "C++",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/javascript")
+async def javascript(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "JavaScript"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "javascript",
+            "lang": "JavaScript",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/rust")
+async def rust(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Rust"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "rust",
+            "lang": "Rust",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/lua")
+async def lua(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Lua"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "lua",
+            "lang": "Lua",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/java")
+async def java(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Java"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "java",
+            "lang": "Java",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/swift")
+async def swift(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Swift"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "swift",
+            "lang": "Swift",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/ruby")
+async def ruby(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Ruby"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "ruby",
+            "lang": "Ruby",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/php")
+async def php(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "PHP"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "php",
+            "lang": "PHP",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/typescript")
+async def typescript(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "TypeScript"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "typescript",
+            "lang": "TypeScript",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/kotlin")
+async def kotlin(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "Kotlin"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "kotlin",
+            "lang": "Kotlin",
+            "idea": idea,
+        },
+    )
+
+
+@app.get("/csharp")
+async def csharp(request: Request):
+    idea = secrets.choice(
+        [idea["idea"] for idea in projectsList if idea["lang"] == "C#"]
+    )
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "category": "c#",
+            "lang": "C#",
+            "idea": idea,
+        },
+    )
+
 
 @app.get("/writing")
 async def writing(request: Request):
